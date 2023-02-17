486b232576f6c503e556e34ef1a706929a9eb6aa
Ivan Covach <eeiprex@gmail.com>
2023-02-18 01:38:18 +0300

    Corrected my mistake in answers of chapter 2

--- task/Chapter_2/2.12 - final test/programm/10_ingredient_regulator.py
+++ task/Chapter_2/2.12 - final test/programm/10_ingredient_regulator.py
@@ -1,12 +1,13 @@
 CUP_SUGAR = 1.5
 CUP_OIL = 1
 CUP_FLOUR = 2.75
+COOKIES = 48
 
 count_bun = int(input("Введите кол-во булочек, которые вы хотите испеч: "))
 
-cup_sugar_for_user = count_bun * CUP_SUGAR / 48
-cup_oil_for_user = count_bun * CUP_OIL / 48
-cup_flour_for_user = count_bun * CUP_FLOUR / 48
+cup_sugar_for_user = count_bun * CUP_SUGAR / COOKIES
+cup_oil_for_user = count_bun * CUP_OIL / COOKIES
+cup_flour_for_user = count_bun * CUP_FLOUR / COOKIES
 
 print(f"Для изготовления {count_bun} булочек нужно:")
 print(f"  1. {cup_sugar_for_user:.2f} стакана сахара.")
