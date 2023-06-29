24ea3a9debd33d8baf2aed0c1365b22adefbfb2b
Ivan Covach <eeiprex@gmail.com>
2023-06-29 20:29:30 +0300

    [complete:ch5.4] Add 2 file and 1 quest

---
+++ core/Chapter_5/5.4_bad_local.py
@@ -0,0 +1,10 @@
+def main():
+    get_name()
+    print(f'Привет {name}.')
+
+
+def get_name():
+    name = input("Введите своё имя: ")
+
+
+main()
---
+++ core/Chapter_5/5.5_birds.py
@@ -0,0 +1,16 @@
+def main():
+    texas()
+    california()
+
+
+def texas():
+    birds = 5000
+    print(f"В Техасе обитает {birds} птиц.")
+
+
+def california():
+    birds = 5000
+    print(f"В Калифорнии обитает {birds} птиц.")
+
+
+main()
